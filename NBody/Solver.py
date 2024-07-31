61745adeec331e46ee3a6ec20ffa50e82a02656b
Chutian Wang <chutian_wang@ucsb.edu>
2024-07-31 13:38:33 -0700

    improved visuals and config logic, added saving option

--- NBody/Solver.py
+++ NBody/Solver.py
@@ -12,6 +12,8 @@ from threading import Thread
 
 COLORS = np.array(['r', 'g', 'b', 'c', 'm', 'y', 'k'])
 MAX_FRAMES = 0xFFFFFFFF
+DEFAULT_FPS = 30
+DEFAULT_MS_PER_FRAME = 10
 
 class Solver(object):
     """
@@ -24,6 +26,33 @@ class Solver(object):
         tsim_current_ms: int
         obj_pos: np.ndarray
         obj_vel: np.ndarray
+
+    @staticmethod
+    def print_dict(d: dict, indent: int = 0) -> None:
+        """
+        Print a dictionary with indentation.
+
+        Args:
+            d (dict): The dictionary to print.
+            indent (int, optional): The indentation level. Defaults to 0.
+        """
+        max_key_length = max(len(str(k)) for k in d.keys())
+    
+        for k, v in d.items():
+            if isinstance(v, dict):
+                print("  " * indent + str(k) + ":")
+                Solver.print_dict(v, indent + 1)
+            elif isinstance(v, list):
+                print("  " * indent + str(k).ljust(max_key_length) + ": [")
+                for item in v:
+                    if isinstance(item, dict):
+                        Solver.print_dict(item, indent + 1)
+                    else:
+                        print("  " * (indent + 1) + str(item))
+                print("  " * indent + "]")
+            else:
+                # Align semicolons
+                print("  " * indent + str(k).ljust(max_key_length) + ": " + str(v))
 
     def __init__(self, config_file=None, visualize=False, precompute = True) -> None:
         """
@@ -54,20 +83,30 @@ class Solver(object):
         self.E = np.sum(self.obj_mass * np.linalg.norm(self.obj_vel, axis=1)**2 / 2)
 
         if self.visualize:
+            print("Visualization properties:")
+            Solver.print_dict(self.visuals, indent=1)
+            print()
             self.fig = plt.figure()
             self.ax = self.fig.add_subplot(111, projection='3d')
             self.ax.set_xlabel('X')
             self.ax.set_ylabel('Y')
             self.ax.set_zlabel('Z')
-            self.ax.set_xlim3d(self.scope[0])
-            self.ax.set_ylim3d(self.scope[1])
-            self.ax.set_zlim3d(self.scope[2])
+            scope = self.visuals.get("scope", [[-1, 1], [-1, 1], [-1, 1]])
+            self.ax.set_xlim3d(scope[0])
+            self.ax.set_ylim3d(scope[1])
+            self.ax.set_zlim3d(scope[2])
             self.ax.set_title("3D Animated Scatter Plot")
             self.history = []
             self.colors = np.random.choice(COLORS, size=(self.obj_pos.shape[0]))
             self.lines = [self.ax.plot([], [], [], color = self.colors[i])[0] for i in range(self.obj_pos.shape[0])]
             self.scat = self.ax.scatter(self.obj_pos[:, 0], self.obj_pos[:, 1], self.obj_pos[:, 2], c=self.colors, marker='o')
-            self.text_box = self.ax.text2D(-0.1, 0.9, '', transform=self.ax.transAxes)
+            self.text_box = self.ax.text2D(-0.2, 0.8, '', transform=self.ax.transAxes)
+            if self.visuals.get("disp_name", False):
+                self.text_annotations = [self.ax.text(self.obj_pos[i, 0], self.obj_pos[i, 1], self.obj_pos[i, 2],
+                                            self.objs[i]["name"], color=self.colors[i]) 
+                                for i in range(self.obj_pos.shape[0])]
+            else:
+                self.text_annotations = None
             self.running = False
 
         if self.precompute:
@@ -116,9 +155,9 @@ class Solver(object):
                 raise StopIteration
             
             if self.visualize:
-                while self.tsim_current*1000 < self.tsim_current_ms + self.ms_per_frame:
+                while self.tsim_current*1000 < self.tsim_current_ms + self.visuals.get("ms_per_frame", DEFAULT_MS_PER_FRAME):
                     self._tick()
-                self.tsim_current_ms += self.ms_per_frame
+                self.tsim_current_ms += self.visuals.get("ms_per_frame", DEFAULT_MS_PER_FRAME)
             else:
                 while self.tsim_current*1000 < self.tsim_current_ms + 1:
                     self._tick()
@@ -149,9 +188,9 @@ class Solver(object):
     def _next_ms(self) -> None:
         
         if self.visualize:
-            while self.tsim_current*1000 < self.tsim_current_ms + self.ms_per_frame:
+            while self.tsim_current*1000 < self.tsim_current_ms + self.visuals.get("ms_per_frame", DEFAULT_MS_PER_FRAME):
                 self._tick()
-            self.tsim_current_ms += self.ms_per_frame
+            self.tsim_current_ms += self.visuals.get("ms_per_frame", DEFAULT_MS_PER_FRAME)
         else:
             while self.tsim_current*1000 < self.tsim_current_ms + 1:
                 self._tick()
@@ -173,9 +212,7 @@ class Solver(object):
             self.objs = self.config["objs"]
             self.desc = self.config["desc"]
             if self.visualize:
-                self.scope = np.array(self.config["scope"])
-                self.ms_per_frame = self.config["ms_per_frame"]
-            self.trace_length = self.config.get("trace_length",20)
+                self.visuals = self.config["visuals"]
             self.config_valid = True
         except:
             print(f"Error loading config from {self.config_file}.")
@@ -187,6 +224,11 @@ class Solver(object):
         Returns:
             Solver: The Solver object itself.
         """
+        # this is to suppress the RuntimeWarning: invalid value encountered in divide
+        # this is due to object self referencing calculating acceleration
+        # this line alone costs 1/16 of the total runtime when problem sizes are small
+        # but it's not a big deal as problem sizes get larger
+        warnings.filterwarnings('ignore', r'invalid value*')
         dt = self.dt
         self.tsim_current += dt
         mask = np.eye(len(self.obj_pos), dtype=bool)
@@ -208,16 +250,30 @@ class Solver(object):
         else:
             data = self
 
-        if data.tsim_current_ms % self.ms_per_frame == 0:
-            self.text_box.set_text(fr"simulation time: {data.tsim_current:.2f} s" + \
-                                   '\n' + fr"$\Delta E = {self.delta_E:.2f} J$" + \
-                                    f"({self.delta_E / self.E:.1f}%)")
+        if data.tsim_current_ms % self.visuals.get("ms_per_frame", DEFAULT_MS_PER_FRAME) == 0:
+            if data.tsim_current_ms == self.tsim_ms:
+                self.text_box.set_text(fr"simulation time: {data.tsim_current:.2f} s" + \
+                                '\n' + fr"$\Delta E = {self.delta_E:.2f} J$" + \
+                                f"({self.delta_E / self.E:.1f}%)\n" + \
+                                "Simulation complete.")
+            else:
+                self.text_box.set_text(fr"simulation time: {data.tsim_current:.2f} s" + \
+                                    '\n' + fr"$\Delta E = {self.delta_E:.2f} J$" + \
+                                    f"({self.delta_E / self.E:.1f}%)\n" + \
+                                    f"{self.visuals.get('ms_per_frame', DEFAULT_MS_PER_FRAME)} ms per frame @ {self.visuals.get('fps', DEFAULT_FPS)} fps")
+            
+            if self.text_annotations:
+                for i, text in enumerate(self.text_annotations):
+                    text.set_position((data.obj_pos[i, 0], data.obj_pos[i, 1]))
+                    text.set_3d_properties(data.obj_pos[i, 2], zdir = (1,1,0))
+                    text.set_text(self.objs[i]["name"])
 
             self.scat._offsets3d = (data.obj_pos[:, 0], data.obj_pos[:, 1], data.obj_pos[:, 2])
             self.history.append(data.obj_pos.copy())
             
             # Limit history length for trace
-            if len(self.history) > self.trace_length:
+            trace_length = self.visuals.get("trace_length", 20)
+            if len(self.history) > trace_length:
                 self.history.pop(0)
                 
             # Update trace lines
@@ -307,16 +363,43 @@ class Solver(object):
                     usr_input = input("Proceed? (y/n): ")
 
         if (not hasattr(self, "ani")) and self.visualize:
-            self.ani = FuncAnimation(self.fig, self._update, frames=self, interval=self.ms_per_frame, blit=False)
+            self.ani = FuncAnimation(self.fig, self._update, frames=self, interval=self.visuals.get("fps", DEFAULT_FPS) , blit=False)
 
-        with warnings.catch_warnings(action="ignore"):
-            try:
-                if self.visualize:
-                    self.running = True
-                    plt.show()
-                else:
-                    for _ in tqdm(self, unit="ms"):
-                        pass
-                    
-            except KeyboardInterrupt:
-                print("Simulation stopped.")
+        try:
+            if self.visualize:
+                self.running = True
+                plt.show()
+            else:
+                for _ in tqdm(self, unit="ms"):
+                    pass
+                
+        except KeyboardInterrupt:
+            print("Simulation stopped.")
+
+    def save(self, filename: str) -> None:
+        """
+        Save the current state of the simulation to a new configuration file.
+        This is for incremental saving of the simulation result.
+        Args:
+            filename (str): The path to the file where the configuration will be saved.
+        """
+        config_data = {
+            "dt": self.dt,
+            "G": self.G,
+            "dtype": self.dtype,
+            "objs": [
+                {
+                    "pos": self.obj_pos[i].tolist(),
+                    "vel": self.obj_vel[i].tolist(),
+                    "mass": self.obj_mass[i],
+                    "name": self.objs[i]["name"]
+                } for i in range(len(self.objs))
+            ],
+            "desc": self.desc
+        }
+        if self.config.get("visuals", None):
+            config_data["visuals"] = self.config["visuals"]
+        
+        with open(filename, 'w') as f:
+            json.dump(config_data, f, indent=4)
+        print(f"Simulation result configuration saved to {filename}")
--- demo.py
+++ demo.py
@@ -1,10 +1,24 @@
 #!./.venv/bin/python3.11
 from NBody import Solver
+import argparse
 
-visulaize = input("Visualize? (y/n): ")
-if visulaize.lower() == "y":
-    mysolver = Solver(visualize=True,precompute=True,config_file="demo_config.json")
-else:
-    mysolver = Solver()
+parser = argparse.ArgumentParser(description='N-Body Simulation')
+parser.add_argument('-c', '--config', metavar='<config_dir>', help='Specify the configuration directory')
+parser.add_argument('-v', '--visualize', action='store_true', help='Visualize the simulation')
+parser.add_argument('-p', '--precompute', action='store_true', help='Precompute the simulation by one max(1ms, 1frame), recommended if visualizing')
+parser.add_argument('-t', '--time', metavar='<time>', type=float, help='The time in seconds for which to run the simulation')
+parser.add_argument('-s', '--save', metavar='<save_dir>', help='Save the simulation result config file to <save_dir>')
 
-mysolver.run(30)
+args = parser.parse_args()
+
+if args.time is None:
+    print("No time specified, defaulting to 30 seconds")
+    args.time = 30
+
+if args.config is None:
+    print("No config specified, using NBody module default config")
+
+solver = Solver(args.config, args.visualize, args.precompute)
+solver.run(args.time)
+if args.save is not None:
+    solver.save(args.save)
