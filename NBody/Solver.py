387c3397ccb177d8b8e645005a7ed414443b9b55
Chutian Wang <chutian_wang@ucsb.edu>
2024-07-30 18:01:21 -0700

    added multithread precompute optimization

--- NBody/Solver.py
+++ NBody/Solver.py
@@ -7,16 +7,25 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
+from dataclasses import dataclass
+from threading import Thread
+
 COLORS = np.array(['r', 'g', 'b', 'c', 'm', 'y', 'k'])
-TRACE_LENGTH = 50
 MAX_FRAMES = 0xFFFFFFFF
 
 class Solver(object):
     """
     N-Body Solver class for simulating gravitational interactions between objects.
     """
+    @dataclass
+    class buffer:
+        tsim_current: float
+        tsim_start_ms: int
+        tsim_current_ms: int
+        obj_pos: np.ndarray
+        obj_vel: np.ndarray
 
-    def __init__(self, config_file=None, visualize=False) -> None:
+    def __init__(self, config_file=None, visualize=False, precompute = True) -> None:
         """
         Initialize the Solver object.
 
@@ -26,6 +35,7 @@ class Solver(object):
         """
         self.config_valid = False
         self.visualize = visualize
+        self.precompute = precompute
         self.tsim_current = 0
         self.tsim_ms = 0
 
@@ -60,6 +70,10 @@ class Solver(object):
             self.text_box = self.ax.text2D(-0.1, 0.9, '', transform=self.ax.transAxes)
             self.running = False
 
+        if self.precompute:
+            self.compute_thread = None
+            self.__next__()
+
     def __iter__(self) -> "Solver":
         """
         Enable iteration over the Solver object.
@@ -80,18 +94,35 @@ class Solver(object):
             StopIteration: If the simulation has reached its end.
         """
 
-        if self.tsim_ms and self.tsim_current * 1000 - self.tsim_start_ms >= self.tsim_ms:
-            self.tsim_start_ms = self.tsim_current_ms
-            raise StopIteration
-        
-        if self.visualize:
-            while self.tsim_current*1000 < self.tsim_current_ms + self.ms_per_frame:
-                self._tick()
-            self.tsim_current_ms += self.ms_per_frame
+        # if precompute is enabled, self is immediately returned
+        # and the next iteration is performed in a separate thread
+        # along with plt's drawing thread (_update)
+        if self.precompute:
+            if self.tsim_ms and self.data_buf.tsim_current * 1000 - self.data_buf.tsim_start_ms >= self.tsim_ms:
+                self.tsim_start_ms = self.tsim_current_ms
+                raise StopIteration
+            
+            # in case the previous thread is still running
+            if self.compute_thread:
+                self.compute_thread.join()
+
+            self.data_buf = self._gen_buffer()
+            self.compute_thread = Thread(target=self._next_ms)
+            self.compute_thread.start()
+            
         else:
-            while self.tsim_current*1000 < self.tsim_current_ms + 1:
-                self._tick()
-            self.tsim_current_ms += 1
+            if self.tsim_ms and self.tsim_current * 1000 - self.tsim_start_ms >= self.tsim_ms:
+                self.tsim_start_ms = self.tsim_current_ms
+                raise StopIteration
+            
+            if self.visualize:
+                while self.tsim_current*1000 < self.tsim_current_ms + self.ms_per_frame:
+                    self._tick()
+                self.tsim_current_ms += self.ms_per_frame
+            else:
+                while self.tsim_current*1000 < self.tsim_current_ms + 1:
+                    self._tick()
+                self.tsim_current_ms += 1
         
         return self
         
@@ -114,7 +145,21 @@ class Solver(object):
             str: A string describing the Solver object.
         """
         return f"N-Body Solver object with {len(self.obj_pos)} objects."
+    
+    def _next_ms(self) -> None:
+        
+        if self.visualize:
+            while self.tsim_current*1000 < self.tsim_current_ms + self.ms_per_frame:
+                self._tick()
+            self.tsim_current_ms += self.ms_per_frame
+        else:
+            while self.tsim_current*1000 < self.tsim_current_ms + 1:
+                self._tick()
+            self.tsim_current_ms += 1
 
+    def _gen_buffer(self) -> None:
+        return self.buffer(self.tsim_current, self.tsim_start_ms, self.tsim_current_ms, self.obj_pos.copy(), self.obj_vel.copy())
+    
     def _read_config(self) -> None:
         """
         Read the configuration from the specified file.
@@ -130,6 +175,7 @@ class Solver(object):
             if self.visualize:
                 self.scope = np.array(self.config["scope"])
                 self.ms_per_frame = self.config["ms_per_frame"]
+            self.trace_length = self.config.get("trace_length",20)
             self.config_valid = True
         except:
             print(f"Error loading config from {self.config_file}.")
@@ -157,25 +203,41 @@ class Solver(object):
     # so we need to declear it as a static method
     @staticmethod
     def _update(self) -> None:
-        if self.tsim_current_ms % self.ms_per_frame == 0:
-            self.text_box.set_text(fr"simulation time: {self.tsim_current:.2f} s" + \
+        if self.precompute:
+            data = self.data_buf
+        else:
+            data = self
+
+        if data.tsim_current_ms % self.ms_per_frame == 0:
+            self.text_box.set_text(fr"simulation time: {data.tsim_current:.2f} s" + \
                                    '\n' + fr"$\Delta E = {self.delta_E:.2f} J$" + \
                                     f"({self.delta_E / self.E:.1f}%)")
 
-            self.scat._offsets3d = (self.obj_pos[:, 0], self.obj_pos[:, 1], self.obj_pos[:, 2])
-            self.history.append(self.obj_pos.copy())
+            self.scat._offsets3d = (data.obj_pos[:, 0], data.obj_pos[:, 1], data.obj_pos[:, 2])
+            self.history.append(data.obj_pos.copy())
             
             # Limit history length for trace
-            if len(self.history) > TRACE_LENGTH:
+            if len(self.history) > self.trace_length:
                 self.history.pop(0)
                 
             # Update trace lines
-            for i in range(self.obj_pos.shape[0]):
+            for i in range(data.obj_pos.shape[0]):
                 trace_data = np.array(self.history)[:, i, :]
                 self.lines[i].set_data(trace_data[:, 0], trace_data[:, 1])
                 self.lines[i].set_3d_properties(trace_data[:, 2])
     
         return self.scat, *self.lines, self.text_box
+    
+    def _build(self) -> None:
+        """
+        Build the initial state of the simulation based on the loaded configuration.
+        """
+        self.obj_pos = np.array([obj["pos"]
+                                 for obj in self.objs], dtype=self.dtype)
+        self.obj_vel = np.array([obj["vel"]
+                                 for obj in self.objs], dtype=self.dtype)
+        self.obj_mass = np.array([obj["mass"]
+                                  for obj in self.objs], dtype=self.dtype)
     
     def load_config(self, config_file: str) -> None:
         """
@@ -187,18 +249,9 @@ class Solver(object):
         self.config_file = config_file
         self._read_config()
         if self.config_valid:
-            self.build()
-
-    def build(self) -> None:
-        """
-        Build the initial state of the simulation based on the loaded configuration.
-        """
-        self.obj_pos = np.array([obj["pos"]
-                                 for obj in self.objs], dtype=self.dtype)
-        self.obj_vel = np.array([obj["vel"]
-                                 for obj in self.objs], dtype=self.dtype)
-        self.obj_mass = np.array([obj["mass"]
-                                  for obj in self.objs], dtype=self.dtype)
+            self._build()
+        else:
+            raise ValueError("Invalid configuration file.")
 
     def set_sim_time(self, time: any) -> None:
         """
@@ -220,10 +273,14 @@ class Solver(object):
         Returns:
             float: The current simulation time in seconds.
         """
+        if self.precompute:
+            return self.data_buf.tsim_current
         return self.tsim_current
     
     @property
     def delta_E(self) -> float:
+        if self.precompute:
+            return self.E - np.sum(self.obj_mass * np.linalg.norm(self.data_buf.obj_vel, axis=1)**2 / 2)
         return self.E - np.sum(self.obj_mass * np.linalg.norm(self.obj_vel, axis=1)**2 / 2)
 
     def run(self, time=None) -> None:
--- demo.py
+++ demo.py
@@ -3,9 +3,8 @@ from NBody import Solver
 
 visulaize = input("Visualize? (y/n): ")
 if visulaize.lower() == "y":
-    mysolver = Solver(visualize=True)
+    mysolver = Solver(visualize=True,precompute=True,config_file="demo_config.json")
 else:
     mysolver = Solver()
 
-mysolver.build()
 mysolver.run(30)
